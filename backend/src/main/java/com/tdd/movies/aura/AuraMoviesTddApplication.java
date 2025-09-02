c6f744006e0966c9c5f027c27eb4fb46f7ecbd08
matheusspacifico <matheuspacifico44@gmail.com>
2025-09-02 16:02:33 -0300

    chore: init backend

---
+++ backend/src/main/java/com/tdd/movies/aura/AuraMoviesTddApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.movies.aura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraMoviesTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuraMoviesTddApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tdd/movies/aura/AuraMoviesTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.movies.aura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraMoviesTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
