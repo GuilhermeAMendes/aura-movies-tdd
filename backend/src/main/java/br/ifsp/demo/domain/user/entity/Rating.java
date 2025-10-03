561d9110ea7f54b29db53b6832494d9ceb3f1cec
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-02 21:03:02 -0300

    feat: create rating entity

---
+++ backend/src/main/java/br/ifsp/demo/domain/user/entity/Rating.java
@@ -0,0 +1,38 @@
+package br.ifsp.demo.domain.user.entity;
+
+import br.ifsp.demo.domain.movie.valueObjects.MovieId;
+import br.ifsp.demo.domain.movie.valueObjects.Note;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Rating {
+    private final UUID id;
+    private final MovieId movieId;
+    private Note note;
+    private LocalDateTime dateOfRating;
+
+    Rating(UUID id, MovieId movieId, Note note, LocalDateTime dateOfRating) {
+        this.id = id;
+        this.movieId = movieId;
+        this.note = note;
+        this.dateOfRating = dateOfRating;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public MovieId getMovieId() {
+        return movieId;
+    }
+
+    public Note getNote() {
+        return note;
+    }
+
+    public LocalDateTime getDateOfRating() {
+        return dateOfRating;
+    }
+
+}
