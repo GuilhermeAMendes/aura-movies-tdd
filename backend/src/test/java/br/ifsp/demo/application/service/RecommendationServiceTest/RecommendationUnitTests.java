c6b6339534edb955b59b629e8979e16ed923c5bb
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-02 21:10:16 -0300

    test: create suite to run all recommendation unit tests

---
+++ backend/src/test/java/br/ifsp/demo/application/service/RecommendationServiceTest/RecommendationUnitTests.java
@@ -0,0 +1,13 @@
+package br.ifsp.demo.application.service.RecommendationServiceTest;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@IncludeTags("UnitTest")
+@SelectPackages("br.ifsp.demo.application.service.RecommendationServiceTest")
+@SuiteDisplayName("All Unit Test of recommendation service")
+public class RecommendationUnitTests {
+}
