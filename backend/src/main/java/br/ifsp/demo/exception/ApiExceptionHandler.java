3001558cf79771aef49318aec9c1b19e2cb1b1c3
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-03 09:47:29 -0300

    feat: add user not found exception to security exception handler

--- backend/src/main/java/br/ifsp/demo/exception/ApiExceptionHandler.java
+++ backend/src/main/java/br/ifsp/demo/exception/ApiExceptionHandler.java
@@ -73,4 +73,16 @@ public class ApiExceptionHandler {
                 .build();
         return new ResponseEntity<>(apiException, conflict);
     }
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException e){
+        final HttpStatus notFound = NOT_FOUND;
+        final ApiException apiException = ApiException.builder()
+                .status(notFound)
+                .message(e.getMessage())
+                .developerMessage(e.getClass().getName())
+                .timestamp(ZonedDateTime.now(ZoneId.of("Z")))
+                .build();
+        return new ResponseEntity<>(apiException, notFound);
+    }
 }
