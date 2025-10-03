12517ee90e50c92a73da010eb0489c06eb44a5f0
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-03 10:52:21 -0300

    test: add scenario 4.4 test

---
+++ backend/src/test/java/br/ifsp/demo/controller/RecommendationControllerTest.java
@@ -0,0 +1,41 @@
+package br.ifsp.demo.controller;
+
+import br.ifsp.demo.security.auth.AuthenticationInfoService;
+import br.ifsp.demo.service.get.GetRecommendationService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = RecommendationController.class)
+public class RecommendationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private AuthenticationInfoService authenticationInfoService;
+
+    @MockitoBean
+    private GetRecommendationService getRecommendationService;
+
+    @Test
+    @Tag("UnitTest")
+    @Tag("TDD")
+    @Tag("[US-4]")
+    @DisplayName("[SC-4.4] - Should return 401 when user is not authenticated")
+    void shouldReturnUnauthorizedWhenNoAuth() throws Exception {
+        mockMvc.perform(get("/api/v1/recommendations")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
+}
+
+
