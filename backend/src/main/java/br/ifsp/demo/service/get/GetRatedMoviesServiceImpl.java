6be8597d1f339151152e4b9f598974f0f1669de7
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-03 17:06:48 -0300

    refactor: add unit test for user with no ratings - closes #24

--- backend/src/main/java/br/ifsp/demo/service/get/GetRatedMoviesServiceImpl.java
+++ backend/src/main/java/br/ifsp/demo/service/get/GetRatedMoviesServiceImpl.java
@@ -1,11 +1,9 @@
 package br.ifsp.demo.service.get;
 
-import br.ifsp.demo.domain.user.Rating;
 import br.ifsp.demo.domain.user.User;
 import br.ifsp.demo.exception.UserNotFoundException;
 import br.ifsp.demo.repository.JpaUserRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public class GetRatedMoviesServiceImpl implements GetRatedMoviesService {
@@ -23,10 +21,6 @@ public class GetRatedMoviesServiceImpl implements GetRatedMoviesService {
             throw new UserNotFoundException("User not found");
         }
         User restoredUser = userOptional.get();
-
-        if (restoredUser.getRatings().isEmpty()) {
-            return new RatedServiceResponseDTO(List.of(new Rating(null, null, null, null)));
-        }
 
         return new RatedServiceResponseDTO(restoredUser.getRatings());
     }
