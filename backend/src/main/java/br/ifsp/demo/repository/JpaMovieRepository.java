4e425bfcbd63f1beed7e95ab03fccf96c1a988f1
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-03 09:47:12 -0300

    fix: fix wrong jpa movie repository id type

--- backend/src/main/java/br/ifsp/demo/repository/JpaMovieRepository.java
+++ backend/src/main/java/br/ifsp/demo/repository/JpaMovieRepository.java
@@ -1,8 +1,9 @@
 package br.ifsp.demo.repository;
 
 import br.ifsp.demo.domain.movie.Movie;
+import br.ifsp.demo.domain.movie.MovieId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaMovieRepository extends JpaRepository<Movie, Long> {
+public interface JpaMovieRepository extends JpaRepository<Movie, MovieId> {
     // findAll() is inherited! No need to declare.
 }
