a6116796b96a2a848a9f3ed78555c291e9d7ff24
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-03 16:35:09 -0300

    feat: create implementation of rated movie service

---
+++ backend/src/main/java/br/ifsp/demo/service/get/GetRatedMoviesServiceImpl.java
@@ -0,0 +1,27 @@
+package br.ifsp.demo.service.get;
+
+import br.ifsp.demo.domain.user.User;
+import br.ifsp.demo.exception.UserNotFoundException;
+import br.ifsp.demo.repository.JpaUserRepository;
+
+import java.util.Optional;
+
+public class GetRatedMoviesServiceImpl implements GetRatedMoviesService {
+    private final JpaUserRepository userRepository;
+
+    public GetRatedMoviesServiceImpl(final JpaUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public RatedServiceResponseDTO restoreRatedMovies(RatedServiceRequestDTO request) {
+        Optional<User> userOptional = userRepository.findUserById(request.userId());
+
+        if (userOptional.isEmpty()) {
+            throw new UserNotFoundException("User not found");
+        }
+        User restoredUser = userOptional.get();
+
+        return new RatedServiceResponseDTO(restoredUser.getRatings());
+    }
+}
