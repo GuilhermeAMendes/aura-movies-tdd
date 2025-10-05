e37049946885811ac4d7de6988065576ebbc30f7
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-05 16:37:07 -0300

    feat: implement logic to check valid limit of note value

--- backend/src/main/java/br/ifsp/demo/domain/movie/Grade.java
+++ backend/src/main/java/br/ifsp/demo/domain/movie/Grade.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Embeddable;
 
 @Embeddable
 public record Grade(int value) {
-    public Grade() {
-        this(0);
+    public Grade {
+        if (value < 0 || value > 5) throw new IllegalArgumentException("Grade must be between 0 and 5");
     }
 }
