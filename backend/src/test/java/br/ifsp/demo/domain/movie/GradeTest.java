c11130a73ab3980843fb3e296cc24f25a34673aa
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-05 18:35:07 -0300

    test: use domain to check interval of valid note - closes #26

--- backend/src/test/java/br/ifsp/demo/domain/movie/GradeTest.java
+++ backend/src/test/java/br/ifsp/demo/domain/movie/GradeTest.java
@@ -11,10 +11,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class GradeTest {
 
     @Tag("UnitTest")
+    @Tag("[US-1]")
     @Tag("[US-3]")
     @ParameterizedTest
     @ValueSource(ints = {-5, -1, 6, 10})
-    @DisplayName("[SC-3.2] - Should throw exception when grade is out of interval [0, 5]")
+    @DisplayName("[SC-1.2, SC-3.2] - Should throw exception when grade is out of interval [0, 5]")
     void shouldThrowExceptionWhenGradeIsOutOfInterval(int invalidGradeValue) {
         assertThatThrownBy(() -> new Grade(invalidGradeValue)).isInstanceOf(IllegalArgumentException.class);
     }
