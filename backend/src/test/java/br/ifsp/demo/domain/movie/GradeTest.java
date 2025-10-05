322d427f05bacda4a3feb59c362270a30c9b64a7
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-05 16:53:18 -0300

    test: add unit test in domain layer to create grade within interval [0,5]

--- backend/src/test/java/br/ifsp/demo/domain/movie/GradeTest.java
+++ backend/src/test/java/br/ifsp/demo/domain/movie/GradeTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class GradeTest {
 
@@ -17,4 +18,15 @@ public class GradeTest {
     void shouldThrowExceptionWhenGradeIsOutOfInterval(int invalidGradeValue) {
         assertThatThrownBy(() -> new Grade(invalidGradeValue)).isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Tag("UnitTest")
+    @Tag("[US-3]")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5})
+    @DisplayName("Should create grade when value is within interval [0, 5]")
+    void shouldCreateGradeWhenValueIsWithinInterval(int validGradeValue) {
+        Grade grade = new Grade(validGradeValue);
+        assertThat(grade.value()).isEqualTo(validGradeValue);
+    }
+
 }
