65679a1654e459388586dbce9be6e1cf0a937ebd
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-05 00:25:58 -0300

    chore: change recommendations controller test name

--- backend/src/test/java/br/ifsp/demo/controller/RecommendationsControllerTest.java
+++ backend/src/test/java/br/ifsp/demo/controller/RecommendationsControllerTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = RecommendationsController.class)
-public class RecommendationControllerTest {
+public class RecommendationsControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
