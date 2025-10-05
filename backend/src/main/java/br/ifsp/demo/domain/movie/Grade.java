f5d6f5a743ba0c2bd7f45f3a77de6237a1c5d6a9
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-05 18:29:41 -0300

    refactor: improve error handling with spec class

--- backend/src/main/java/br/ifsp/demo/domain/movie/Grade.java
+++ backend/src/main/java/br/ifsp/demo/domain/movie/Grade.java
@@ -1,10 +1,11 @@
 package br.ifsp.demo.domain.movie;
 
+import br.ifsp.demo.exception.InvalidNoteInterval;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
 public record Grade(int value) {
     public Grade {
-        if (value < 0 || value > 5) throw new IllegalArgumentException("Grade must be between 0 and 5");
+        if (value < 0 || value > 5) throw new InvalidNoteInterval("Grade must be between 0 and 5");
     }
 }
