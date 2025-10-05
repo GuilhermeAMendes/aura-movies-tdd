63caf6817935521c43e9ea3adfaca10d2c1b6459
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-05 18:13:45 -0300

    feat: implement save rate service - closes #25

---
+++ backend/src/main/java/br/ifsp/demo/service/post/PostRateServiceImpl.java
@@ -0,0 +1,31 @@
+package br.ifsp.demo.service.post;
+
+import br.ifsp.demo.domain.user.Rating;
+import br.ifsp.demo.domain.user.User;
+import br.ifsp.demo.exception.UserNotFoundException;
+import br.ifsp.demo.repository.JpaMovieRepository;
+import br.ifsp.demo.repository.JpaUserRepository;
+
+public class PostRateServiceImpl implements PostRateService {
+    private final JpaUserRepository userRepository;
+    private final JpaMovieRepository movieRepository;
+
+    public PostRateServiceImpl(JpaUserRepository userRepository, JpaMovieRepository movieRepository) {
+        this.userRepository = userRepository;
+        this.movieRepository = movieRepository;
+    }
+
+    @Override
+    public PostRateServiceResponseDTO saveRate(PostRateServiceRequestDTO request) {
+        User user = userRepository.findById(request.userId()).orElseThrow(() ->
+                new UserNotFoundException("User not found")
+        );
+
+        if (movieRepository.findById(request.movieId()).isEmpty()) return null;
+
+        Rating rating = user.addRating(request.movieId(), request.grade());
+        userRepository.save(user);
+
+        return new PostRateServiceResponseDTO(rating);
+    }
+}
