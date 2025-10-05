41e2e16880680a5577cd47adc6b01097e09986a2
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-05 16:34:50 -0300

    test: add unit test to check limit in value of note

---
+++ backend/src/test/java/br/ifsp/demo/domain/movie/GradeTest.java
@@ -0,0 +1,19 @@
+package br.ifsp.demo.domain.movie;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class GradeTest {
+
+    @Tag("UnitTest")
+    @ParameterizedTest
+    @ValueSource(ints = {-5, -1, 6, 10})
+    @DisplayName("Should throw exception when grade is out of interval [0, 5]")
+    void shouldThrowExceptionWhenGradeIsOutOfInterval(int invalidGradeValue) {
+        assertThatThrownBy(() -> new Grade(invalidGradeValue)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
