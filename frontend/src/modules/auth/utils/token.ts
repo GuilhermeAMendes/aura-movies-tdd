9a4c73713d4175c3e53d304e30e8e9c7cbadcc36
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-27 19:26:47 -0300

    feat: create util to decode token

---
+++ frontend/src/modules/auth/utils/token.ts
@@ -0,0 +1,13 @@
+// External Library
+import { jwtDecode } from "jwt-decode";
+
+// Types
+import type { JWTClaims } from "@types/token";
+
+export const decodeToken = (tokenEntry: string): JWTClaims => {
+  return jwtDecode<JWTClaims>(tokenEntry);
+};
+
+export const isValidToken = (tokenEntry: JWTClaims): boolean => {
+  return tokenEntry.exp * 1000 > Date.now();
+};
