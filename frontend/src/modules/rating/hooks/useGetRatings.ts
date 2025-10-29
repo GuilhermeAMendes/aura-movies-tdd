c66a05e3ec2a3747d0d4871f3f7f4523b0bf82fd
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-28 22:51:31 -0300

    feat: create custom hook to restore all movies with rating

---
+++ frontend/src/modules/rating/hooks/useGetRatings.ts
@@ -0,0 +1,65 @@
+"use client";
+
+// External Library
+import { useState, useEffect } from "react";
+import { toast } from "sonner";
+
+// Store
+import { useAuthStore } from "@/modules/auth/store/authStore";
+
+// Service
+import getRatingsService from "../service/getRatings";
+
+// Error
+import { ApplicationError } from "@/shared/errors/base/ApplicationError";
+
+// Type Guard
+import { isLeft } from "@/shared/patterns/either";
+
+// Types
+import type { Rating } from "../types";
+
+interface UseGetRatingsResponse {
+  ratings: Rating[];
+  isLoading: boolean;
+  error: ApplicationError | null;
+}
+
+export function useGetRatings(): UseGetRatingsResponse {
+  const [ratings, setRatings] = useState<Rating[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<ApplicationError | null>(null);
+
+  const { token } = useAuthStore();
+
+  const loadRatings = async () => {
+    setIsLoading(true);
+    setError(null);
+
+    const result = await getRatingsService();
+
+    if (isLeft(result)) {
+      setError(result.value);
+      toast.error("Erro ao buscar avaliações", {
+        description: result.value.message,
+      });
+      setIsLoading(false);
+      return;
+    }
+
+    setRatings(result.value.ratings);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (!token) {
+      setIsLoading(false);
+      setRatings([]);
+      return;
+    }
+
+    loadRatings();
+  }, [token]);
+
+  return { ratings, isLoading, error };
+}
