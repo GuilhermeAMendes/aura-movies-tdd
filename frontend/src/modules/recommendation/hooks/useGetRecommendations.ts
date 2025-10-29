4271a902fcef31ff4196bdb835ce1f6d4e4192aa
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-29 13:34:29 -0300

    feat: create custom hook to restore recommendations to the user

---
+++ frontend/src/modules/recommendation/hooks/useGetRecommendations.ts
@@ -0,0 +1,66 @@
+"use client";
+
+// External Library
+import { useState, useEffect } from "react";
+import { toast } from "sonner";
+
+// Store
+import { useAuthStore } from "@/modules/auth/store/authStore";
+
+// Service
+import getRecommendationsService from "../service/getRecommendations";
+
+// Error
+import { ApplicationError } from "@/shared/errors/base/ApplicationError";
+
+// Type Guard
+import { isLeft } from "@/shared/patterns/either";
+
+// Types
+import type { Movie } from "@/modules/movie/types";
+
+interface UseGetRecommendationsResponse {
+  movies: Movie[];
+  isLoading: boolean;
+  error: ApplicationError | null;
+}
+
+export function useGetRecommendations(): UseGetRecommendationsResponse {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<ApplicationError | null>(null);
+
+  const { token, isSessionRestored } = useAuthStore();
+
+  const loadRecommendations = async () => {
+    setIsLoading(true);
+    setError(null);
+
+    const result = await getRecommendationsService();
+
+    if (isLeft(result)) {
+      setError(result.value);
+      toast.error("Erro ao buscar recomendações", {
+        description: result.value.message,
+      });
+      setIsLoading(false);
+      return;
+    }
+
+    setMovies(result.value.recommendations);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (!isSessionRestored) return;
+    if (!token) {
+      setIsLoading(false);
+      setMovies([]);
+      return;
+    }
+
+    loadRecommendations();
+  }, [token, isSessionRestored]);
+
+  return { movies, isLoading, error };
+}
