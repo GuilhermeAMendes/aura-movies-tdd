b122294cf53368d8b520a210e08d6d9f0689427c
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-28 21:06:53 -0300

    feat: create rating types to the services

---
+++ frontend/src/modules/rating/types.ts
@@ -0,0 +1,18 @@
+// Types
+import type { MovieId } from "../movie/types";
+
+export interface Rating {
+  movieId: MovieId;
+  grade: number;
+  lastGradedAt: string;
+}
+
+export interface GetRatingResponse {
+  ratings: Rating[];
+}
+
+export interface PostRatingPayload extends Omit<Rating, "lastGradedAt"> {}
+
+export interface PostRatingResponse {
+  rating: Rating;
+}
