60a081b34fdeba654aff22d478222f0093616b59
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-30 19:37:37 -0300

    refactor: add user id protection to get movies by id route

--- backend/src/main/java/br/ifsp/demo/controller/MovieController.java
+++ backend/src/main/java/br/ifsp/demo/controller/MovieController.java
@@ -1,6 +1,7 @@
 package br.ifsp.demo.controller;
 
 import br.ifsp.demo.domain.movie.MovieId;
+import br.ifsp.demo.security.auth.AuthenticationInfoService;
 import br.ifsp.demo.service.get.GetMovieByIdService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,18 +11,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 @RequiredArgsConstructor
 public class MovieController {
 
+    private final AuthenticationInfoService authenticationInfoService;
     private final GetMovieByIdService getMovieByIdService;
 
     @GetMapping("/{id}")
     public ResponseEntity<GetMovieByIdService.GetMovieByIdResponseDTO> getMovieById(@PathVariable String id) {
+        UUID userId = authenticationInfoService.getAuthenticatedUserId();
         MovieId movieId = new MovieId(id);
 
-        var request = new GetMovieByIdService.GetMovieByIdRequestDTO(movieId);
+        var request = new GetMovieByIdService.GetMovieByIdRequestDTO(userId, movieId);
         var response = getMovieByIdService.getMovieById(request);
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
--- backend/src/main/java/br/ifsp/demo/service/get/GetMovieByIdService.java
+++ backend/src/main/java/br/ifsp/demo/service/get/GetMovieByIdService.java
@@ -3,11 +3,14 @@ package br.ifsp.demo.service.get;
 import br.ifsp.demo.domain.movie.Movie;
 import br.ifsp.demo.domain.movie.MovieId;
 
+import java.util.UUID;
+
 public interface GetMovieByIdService {
 
     GetMovieByIdResponseDTO getMovieById(GetMovieByIdRequestDTO request);
 
     record GetMovieByIdRequestDTO(
+            UUID userId,
             MovieId movieId
     ) {
     }
