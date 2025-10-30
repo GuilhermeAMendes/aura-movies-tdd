4c2fd96d288a733c577082df473fc6807a859469
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-30 19:50:25 -0300

    chore: annotate get all movies service with transactional

--- backend/src/main/java/br/ifsp/demo/service/get/GetAllMoviesServiceImpl.java
+++ backend/src/main/java/br/ifsp/demo/service/get/GetAllMoviesServiceImpl.java
@@ -7,6 +7,7 @@ import br.ifsp.demo.repository.JpaMovieRepository;
 import br.ifsp.demo.repository.JpaUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,8 +19,8 @@ public class GetAllMoviesServiceImpl implements GetAllMoviesService {
     private final JpaMovieRepository movieRepository;
     private final JpaUserRepository userRepository;
 
-
     @Override
+    @Transactional(readOnly = true)
     public GetAllMoviesResponseDTO getAllMovies(GetAllMoviesRequestDTO request) {
 
         Optional<User> user = userRepository.findById(request.userId());
