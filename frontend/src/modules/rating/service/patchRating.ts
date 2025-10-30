e90e4b8efcf377f5791e830fa1d47f6d22644a93
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-30 17:38:45 -0300

    feat: implement service to patch rate

---
+++ frontend/src/modules/rating/service/patchRating.ts
@@ -0,0 +1,37 @@
+// Infra
+import { AxiosClient } from "@/infra/http/axios/axiosClient";
+import { isAxiosError } from "axios";
+
+// Type Guard
+import { type Either, right } from "@/shared/patterns/either";
+
+// Error
+import { ApplicationError } from "@/shared/errors/base/ApplicationError";
+
+// Factory
+import errorFactory from "@/shared/errors/factory/errorFactory";
+
+// Types
+import type { PatchRatingPayload } from "../types";
+
+export default async function patchRating(
+  payload: PatchRatingPayload
+): Promise<Either<ApplicationError, void>> {
+  try {
+    const { data: response } = await AxiosClient.post<void>("ratings", payload);
+    return right(response);
+  } catch (error) {
+    let message = "Update rating failed";
+
+    if (isAxiosError(error)) {
+      message =
+        error.response?.data?.detail ||
+        error.response?.data?.message ||
+        error.message;
+    } else if (error instanceof Error) {
+      message = error.message;
+    }
+
+    return errorFactory("custom", message);
+  }
+}
