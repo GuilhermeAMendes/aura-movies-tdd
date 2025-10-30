aee8bf332b301ebd6480bf9b9a261d16ce8ec8ca
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-30 18:50:57 -0300

    feat: create utils to formatter label of movie genre

---
+++ frontend/src/modules/movie/utils/formatterLabel.ts
@@ -0,0 +1,18 @@
+// Types
+import type { Genre } from "../types";
+
+// Constants
+export const genreLabelMap: Record<Genre, string> = {
+  ACTION: "Ação",
+  ADVENTURE: "Aventura",
+  COMEDY: "Comédia",
+  DOCUMENTARY: "Documentário",
+  DRAMA: "Drama",
+  FANTASY: "Fantasia",
+  HORROR: "Terror",
+  ROMANCE: "Romance",
+  SCI_FI: "SCI-FI",
+  THRILLER: "Suspense",
+};
+
+export default genreLabelMap;
