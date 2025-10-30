9448e6e7a52c36b466a4c9322e9bc122b7947a4c
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-30 20:18:53 -0300

    chore: move user controller back to security folder

--- backend/src/main/java/br/ifsp/demo/security/auth/UserController.java
+++ backend/src/main/java/br/ifsp/demo/security/auth/UserController.java
@@ -1,6 +1,5 @@
-package br.ifsp.demo.controller;
+package br.ifsp.demo.security.auth;
 
-import br.ifsp.demo.security.auth.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
