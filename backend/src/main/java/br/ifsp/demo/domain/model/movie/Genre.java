5d08fb3b410cc2db968fcc47d796198ef088f8e9
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-31 18:52:33 -0300

    refactor: remove unnecessary method in enumeration of genre

--- backend/src/main/java/br/ifsp/demo/domain/model/movie/Genre.java
+++ backend/src/main/java/br/ifsp/demo/domain/model/movie/Genre.java
@@ -4,28 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum Genre {
-    ACTION("Action"),
-    ADVENTURE("Adventure"),
-    COMEDY("Comedy"),
-    DRAMA("Drama"),
-    FANTASY("Fantasy"),
-    HORROR("Horror"),
-    ROMANCE("Romance"),
-    SCI_FI("Sci-Fi"),
-    THRILLER("Thriller"),
-    DOCUMENTARY("Documentary");
-    private final String label;
-
-    Genre(final String label) {
-        this.label = label;
-    }
-
-    public static Genre fromLabel(final String label) {
-        for (final Genre genre : Genre.values()) {
-            if (genre.getLabel().equals(label)) {
-                return genre;
-            }
-        }
-        return null;
-    }
+    ACTION,
+    ADVENTURE,
+    COMEDY,
+    DRAMA,
+    FANTASY,
+    HORROR,
+    ROMANCE,
+    SCI_FI,
+    THRILLER,
+    DOCUMENTARY;
 }
