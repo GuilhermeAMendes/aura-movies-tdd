3e0c5b5292af94d95fdccb3a5ce87194b7b55084
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-31 14:24:03 -0300

    feat: create endpoint to delete rate of the movie

--- backend/src/main/java/br/ifsp/demo/domain/controller/RatingsController.java
+++ backend/src/main/java/br/ifsp/demo/domain/controller/RatingsController.java
@@ -1,9 +1,10 @@
 package br.ifsp.demo.domain.controller;
 
-import br.ifsp.demo.security.auth.AuthenticationInfoService;
+import br.ifsp.demo.domain.service.delete.DeleteRateService;
 import br.ifsp.demo.domain.service.get.GetRatedMoviesService;
 import br.ifsp.demo.domain.service.patch.PatchRateService;
 import br.ifsp.demo.domain.service.post.PostRateService;
+import br.ifsp.demo.security.auth.AuthenticationInfoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,7 @@ public class RatingsController {
     private final GetRatedMoviesService getRatedMoviesService;
     private final PostRateService postRateService;
     private final PatchRateService patchRateService;
+    private final DeleteRateService deleteRateService;
 
     @GetMapping
     public ResponseEntity<GetRatedMoviesService.RatedServiceResponseDTO> getRatedMoviesFromUser() {
@@ -53,5 +55,18 @@ public class RatingsController {
         );
         var response = patchRateService.patchRate(request);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<DeleteRateService.DeleteRateServiceResponseDTO> deleteRatedMovies(
+            @RequestBody DeleteRateService.DeleteRateServiceRequestDTO deleteRateRequest
+    ) {
+        UUID userId = authenticationInfoService.getAuthenticatedUserId();
+        var request = new DeleteRateService.DeleteRateServiceRequestDTO(
+                userId,
+                deleteRateRequest.movieId()
+        );
+        deleteRateService.deleteRate(request);
+        return ResponseEntity.noContent().build();
     }
 }
