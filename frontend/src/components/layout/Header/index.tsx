e5caeee112d7faa8d7c2993480962c09602d0e0a
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-30 22:34:23 -0300

    feat: integrate logout button

--- frontend/src/components/layout/Header/index.tsx
+++ frontend/src/components/layout/Header/index.tsx
@@ -6,16 +6,19 @@ import { usePathname } from "next/navigation";
 
 // Components
 import { Button } from "@/components/ui/button";
+import { LogoutButton } from "@/modules/auth/components/buttons/LogoutButton";
 
-// Utils
+// Hooks
+import { useAuthStore } from "@/modules/auth/store/authStore";
 import { useNavigationHandler } from "@/shared/hooks/navigation/useNavigation";
 
 export function Header() {
   const pathname = usePathname();
   const { navigateTo } = useNavigationHandler();
 
-  const isHomePage = pathname === "/";
+  const { token } = useAuthStore();
 
+  const isHomePage = pathname === "/";
   const isAuthPage = pathname === "/login";
 
   if (isAuthPage) {
@@ -28,15 +31,16 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="container flex h-16 items-center justify-between py-4 px-6 md:px-8">
+      <div className="flex h-16 items-center justify-between px-6">
         <Link href="/" className="flex items-center space-x-2">
           <span className="font-bold text-2xl tracking-tight text-primary">
             Aura
           </span>
         </Link>
-
         <nav>
-          {isHomePage && (
+          {token ? (
+            <LogoutButton />
+          ) : isHomePage ? (
             <Button
               onClick={handleLoginClick}
               variant="ghost"
@@ -44,7 +48,7 @@ export function Header() {
             >
               Login
             </Button>
-          )}
+          ) : null}
         </nav>
       </div>
     </header>
