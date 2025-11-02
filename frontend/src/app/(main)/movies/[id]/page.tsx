0b72871ec052837354d2aca3110010c602f650cd
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-11-02 20:43:30 -0300

    refactor: clear dead code

--- frontend/src/app/(main)/movies/[id]/page.tsx
+++ frontend/src/app/(main)/movies/[id]/page.tsx
@@ -78,21 +78,7 @@ export default function MovieDetailPage() {
                 {movie.title}
               </h1>
               <GenericSynopsis movie={movie} />
-              <div className="flex gap-1">
-                {/* {rating ? (
-                  <div className="flex items-center gap-1">
-                    <StarIcon className="w-5 h-5 text-yellow-500 fill-yellow-500" />
-                    <span className="font-bold text-lg">{rating.grade}</span>
-                  </div>
-                ) : (
-                  <div className="flex items-center gap-1">
-                    <StarIcon className="w-5 h-5 text-gray-400 fill-gratext-gray-400" />
-                    <span className="font-bold text-lg">
-                      Filme não avaliado
-                    </span>
-                  </div>
-                )} */}
-              </div>
+              <div className="flex gap-1"></div>
               <MovieRatingForm
                 movieId={movie.movieId.id}
                 existingRating={rating}
