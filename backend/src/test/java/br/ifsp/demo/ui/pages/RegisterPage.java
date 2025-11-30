b01e249d09a414e1a187105b3eda7abaca280569
Matheus <mgusmao.dev@gmail.com>
2025-11-30 17:59:31 -0300

    test(ui/pages): prepare tests for the registration page

---
+++ backend/src/test/java/br/ifsp/demo/ui/pages/RegisterPage.java
@@ -0,0 +1,31 @@
+package br.ifsp.demo.ui.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Objects;
+
+public class RegisterPage extends BasePage {
+    private final By firstNameInput = By.xpath("//input[@name='name']");
+    private final By lastNameInput = By.xpath("//input[@name='lastname']");
+    private final By emailInput = By.xpath("//input[@name='email']");
+    private final By passwordInput = By.xpath("//input[@name='password']");
+    private final By registerButton = By.xpath("//button[text()='Registrar']");
+
+    public RegisterPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public void open() {
+        driver.get(Objects.requireNonNull(driver.getCurrentUrl()).replaceAll("/.*$", "") + "/register");
+    }
+
+    public void performRegister(String firstName, String lastName, String email, String password) {
+        type(firstNameInput, firstName);
+        type(lastNameInput, lastName);
+        type(emailInput, email);
+        type(passwordInput, password);
+        click(registerButton);
+    }
+}
