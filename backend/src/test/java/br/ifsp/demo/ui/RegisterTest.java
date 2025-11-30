67d249a33a586f5af5cf47fc744d47a22d21c106
Matheus Gusmão <mgusmao.dev@gmail.com>
2025-11-30 20:38:47 -0300

    test(ui): create failed registration test because the fields are empty

--- backend/src/test/java/br/ifsp/demo/ui/RegisterTest.java
+++ backend/src/test/java/br/ifsp/demo/ui/RegisterTest.java
@@ -5,6 +5,8 @@ import br.ifsp.demo.ui.utils.pages.RegisterPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.By;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,5 +35,24 @@ public class RegisterTest extends BaseTest {
         final By successToast = By.xpath("//div[contains(text(), 'Registro bem-sucedido!')]");
 
         assertThat(successToast).isNotNull();
+    }
+
+    @Tag("UiTest")
+    @ParameterizedTest(name = "Teste {index}: Campo [{4}] deve falhar com erro: {5}")
+    @CsvSource({
+            "'', gusmao, a@b.com, 123, name, O nome é obrigatório",
+            "matheus, '', a@b.com, 123, lastname, O sobrenome é obrigatório",
+            "matheus, gusmao, '', 123, email, 'Por favor, insira um e-mail válido.'",
+            "matheus, gusmao, a@b.com, '', password, A senha é obrigatória"
+    })
+    void registrationFailedBecauseEmptyFieldsTest(String firstName, String lastName, String email, String password, String fieldName, String expectedMessage) {
+        RegisterPage registerPage = new RegisterPage(driver, wait);
+
+        registerPage.performRegister(firstName, lastName, email, password);
+
+        final String erroMessage = registerPage.getFieldErrorMessage(fieldName);
+
+        assertThat(driver.getCurrentUrl()).contains("register");
+        assertThat(erroMessage).isEqualTo(expectedMessage);
     }
 }
