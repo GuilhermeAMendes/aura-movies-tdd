d498c1956012614cfebacf2162d2aa70acc5efbf
Matheus Gusmão <mgusmao.dev@gmail.com>
2025-11-30 20:38:12 -0300

    test(ui/util): move the util function to register page

--- backend/src/test/java/br/ifsp/demo/ui/utils/pages/RegisterPage.java
+++ backend/src/test/java/br/ifsp/demo/ui/utils/pages/RegisterPage.java
@@ -1,7 +1,10 @@
 package br.ifsp.demo.ui.utils.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class RegisterPage extends BasePage {
@@ -14,6 +17,23 @@ public class RegisterPage extends BasePage {
 
     public RegisterPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
+    }
+
+    public String getFieldErrorMessage(String inputName) {
+        final String xpath = String.format(
+                "//input[@name='%s']/following-sibling::p[@data-slot='form-message']",
+                inputName
+        );
+
+        try {
+            WebElement errorElement = wait.until(
+                    ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath))
+            );
+            return errorElement.getText();
+        } catch (TimeoutException e) {
+            System.out.println("Não encontrou mensagem de erro para o campo: " + inputName);
+            throw e;
+        }
     }
 
     public void performRegister(String firstName, String lastName, String email, String password) {
