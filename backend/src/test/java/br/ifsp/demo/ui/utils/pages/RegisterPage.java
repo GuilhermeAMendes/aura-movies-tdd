94799f563de5bc13b28c83a42605c820fb888eff
Matheus Gusmão <mgusmao.dev@gmail.com>
2025-11-30 20:10:42 -0300

    test(ui/pages): fix registration step to step

--- backend/src/test/java/br/ifsp/demo/ui/utils/pages/RegisterPage.java
+++ backend/src/test/java/br/ifsp/demo/ui/utils/pages/RegisterPage.java
@@ -4,32 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.Objects;
-
 public class RegisterPage extends BasePage {
+    private final By toRegisterScreenButton = By.xpath("//button[text()='Registrar']");
     private final By firstNameInput = By.xpath("//input[@name='name']");
     private final By lastNameInput = By.xpath("//input[@name='lastname']");
     private final By emailInput = By.xpath("//input[@name='email']");
     private final By passwordInput = By.xpath("//input[@name='password']");
-    private final By registerButton = By.xpath("//button[text()='Registrar']");
+    private final By registerFormButton = By.xpath("//button[text()='Registrar']");
 
     public RegisterPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
 
-    public void open() {
-        driver.get(Objects.requireNonNull(driver.getCurrentUrl()).replaceAll("/.*$", "") + "/register");
-    }
-
     public void performRegister(String firstName, String lastName, String email, String password) {
+        click(toRegisterScreenButton);
+        waitForUrlToBe(FRONT_URL + "register");
         type(firstNameInput, firstName);
         type(lastNameInput, lastName);
         type(emailInput, email);
         type(passwordInput, password);
-        click(registerButton);
-    }
-
-    public String getToastMessagePart(String expectedPart) {
-        return waitForToastContaining(expectedPart, 5);
+        click(registerFormButton);
     }
 }
