d262a14e3e7c3dd9b554e009dc2b9121e8c99477
MuriloCSS <murilosao06@gmail.com>
2025-12-01 16:10:31 -0300

    test: add parameterized integration test for PATCH /ratings/{movieId} with invalid grade values

--- backend/src/test/java/br/ifsp/demo/domain/controller/integration/RatingsControllerIntegrationTest.java
+++ backend/src/test/java/br/ifsp/demo/domain/controller/integration/RatingsControllerIntegrationTest.java
@@ -277,4 +277,34 @@ public class RatingsControllerIntegrationTest {
                 .andExpect(jsonPath("$.message").value(exceptionMessage))
                 .andExpect(jsonPath("$.status").value("NOT_FOUND"));
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-5, -1, 6, 10})
+    @Tag("ApiTest")
+    @Tag("IntegrationTest")
+    @DisplayName("PATCH /ratings/{movieId} - Should return 400 when patching rating with invalid grade interval")
+    void shouldReturn400WhenPatchingRatingWithInvalidGradeInterval(int grade) throws Exception {
+        MovieId movieId = new MovieId(UUID.randomUUID());
+        User mockUser = createUserMock();
+        String exceptionMessage = "Grade must be between 0 and 5";
+
+        when(authenticationInfoService.getAuthenticatedUserId()).thenReturn(mockUser.getId());
+
+        String jsonContent = String.format(
+                """
+                        {
+                          "grade": "%d"
+                        }""",
+                grade
+        );
+
+        mockMvc.perform(patch("/api/v1/ratings/{movieId}", movieId.unwrap())
+                        .with(SecurityMockMvcRequestPostProcessors.user(mockUser))
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonContent))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(exceptionMessage))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"));
+    }
 }
